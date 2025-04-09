68bca5b4804f8fb08e62073aa6942790a1fa89f4
anil168168 <clggithub@gmail.com>
2025-04-09 10:13:11 +0530

    datatypes

---
+++ datatypes.java
@@ -0,0 +1,17 @@
+public class datatypes {
+    public static void main(String[] args) {
+        int a = 5;               
+        float b = 5.99f;    
+        char c = 'D';         
+        boolean d = true;       
+        String e = "Hello"; 
+
+        System.out.println("int:"+a);
+        System.out.println("float:"+b);
+        System.out.println("char:"+c);
+        System.out.println("boolean:"+d);
+        System.out.println("int:"+e);
+        
+
+    }
+}
